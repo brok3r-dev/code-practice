7608aa0e434b9e6bd5ca3a8ae24062e9fcd8b120
unknown <brok3r@naver.com>
2022-06-07 11:49:14 +0900

    commit: 2022-06-07

--- 주차 요금/Solution.kt
+++ 주차 요금/Solution.kt
@@ -1,9 +1,7 @@
-import java.time.format.DateTimeFormatter
-import java.time.LocalDateTime
-
 class Solution {
     fun solution(fees: IntArray, records: Array<String>): IntArray {
         val lot = mutableMapOf<String, String>()
+        val totalTime = mutableMapOf<String, Int>()
         val paid = mutableMapOf<String, Int>()
 
         val defaultTime = fees[0]
@@ -13,27 +11,34 @@ class Solution {
 
         records.forEach { r ->
             val record = r.split(" ")
-
-            lot.get(record[1]).let { inTime ->
-                var duration = checkTime(inTime, record[0])
-
-                paid.put(record[1], paid.getOrDefault(record[1], 0) + defaultFee)
-                if (duration > defaultTime) {
-                    duration -= defaultTime
-
-                }
-
+            lot.get(record[1])?.let { inTime ->
+                totalTime.put(record[1], totalTime.getOrDefault(record[1], 0) + checkTime(inTime, record[0]))
                 lot.remove(record[1])
-            } ?: {
+            } ?: run {
                 lot.put(record[1], record[0])
             }
         }
 
-        var answer: IntArray = intArrayOf()
-        return answer
+        lot.forEach { l ->
+            totalTime.put(l.key, totalTime.getOrDefault(l.key, 0) + checkTime(l.value, "23:59"))
+        }
+
+        totalTime.forEach { t ->
+            paid.put(t.key, paid.getOrDefault(t.key, 0) + defaultFee)
+            var duration = t.value - defaultTime
+            while(duration > 0) {
+                paid.put(t.key, paid.getOrDefault(t.key, 0) + intervalFee)
+                duration -= interval
+            }
+        }
+
+        return paid.toSortedMap().values.toIntArray()
     }
 
-    fun checkTime(inTime: String?, outTime: String): Long {
-        return 0
+    fun checkTime(incoming: String, outgoing: String): Int {
+        val inTime = incoming.split(":")
+        val outTime = outgoing.split(":")
+
+        return 60 * (outTime[0].toInt() - inTime[0].toInt()) + (outTime[1].toInt() - inTime[1].toInt())
     }
 }
